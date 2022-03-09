7f0e45a318fea640f957324dd07cdd6244514254
bakisimba <99473514+bakisimba@users.noreply.github.com>
2022-03-09 14:33:33 +0200

    Add files via upload
    my assignment

---
+++ main.js
@@ -0,0 +1,16 @@
+function gret(){
+    document.getElementById("fet").style.display='none';
+    document.getElementById("flip").style.display='none';
+    document.getElementById("fet").style.display='block';
+    document.getElementById("flit").style.display='none';
+}
+function work(){
+    document.getElementById("flit").style.display='block';
+    document.getElementById("fet").style.display='none';
+    document.getElementById("flip").style.display='none';
+}
+function blit(){
+    document.getElementById("flip").style.display='block';
+    document.getElementById("fet").style.display='none';
+    document.getElementById("flit").style.display='none';
+}
